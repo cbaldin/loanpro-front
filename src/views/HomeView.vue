18f2d9ecdb7c8458a7ebacda95411474ef727b45
cbaldin <cristiano.baldin@gmail.com>
2023-02-26 18:26:59 -0300

    adjust

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -75,7 +75,7 @@
           <div class="dropdown">
             <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
               aria-expanded="false">
-              Number os itens: {{ paginationSize }}
+              Number of itens: {{ paginationSize }}
             </button>
             <ul class="dropdown-menu">
               <li><button class="dropdown-item"
