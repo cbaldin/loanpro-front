eecae62ed939edbb97fe36e71bc6e6bdf395f876
cbaldin <cristiano.baldin@gmail.com>
2023-02-26 23:12:27 -0300

    Remove console.log

--- src/router/index.js
+++ src/router/index.js
@@ -29,16 +29,11 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  console.log('passou pelo router' + to + ' @ ' + from)
   if (to.meta?.auth) {
     const auth = useAuth()
-    console.log('User token, eh o: ' + auth.token)
-    console.log('O auth.user é o -> ' + auth.user)
     if (auth.token && auth.user) {
       const isAuthenticated = auth.checkToken()
-      console.log('passou pelo router 3' + to)
       if (isAuthenticated) {
-        console.log('passou pelo router 4' + to)
         next()
       } else {
         next({ name: 'login' })
